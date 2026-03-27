db57c2a6e6730baf18ee0ec795eab208b6911c3e
Paulo André <p.souz436@gmail.com>
2026-03-26 21:50:11 -0300

    codigos da aula2

---
+++ aulas/aula02/src/App.jsx
@@ -0,0 +1,41 @@
+import './App.css';
+import logo from './assets/learn.svg'
+
+function Cabecalho() {
+  return(
+    <header>
+      <h1>Titulo</h1>
+    </header>
+  )
+}
+
+function App (){
+  //   <div>
+//     <Cabecalho/>
+//      <div>{2+2}</div>
+//      <img src={logo} alt="" />
+//      <p></p>
+//   </div>
+// 
+  return (
+    
+
+<main>
+      <img src={logo} alt="Icone de um chapeu de estudante" />
+      <h1>Aluno Online</h1> 
+      <label htmlFor="matricula">Matricula</label>
+      <input type="number" id="matricula"
+       name="matricula" />
+       <p id = "matriculaErro"></p>
+      <label htmlFor="senha">Senha</label>
+      <input type="password" id="senha"
+       name="senha" />
+       <p id = "senhaErro"></p>
+      <button type="submit">Entrar</button>
+    </main>
+    );  
+
+  }
+
+
+export default App;
