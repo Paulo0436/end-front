dcfe4c8611c512c7ccb3de894faf6e1b447ab814
Paulo André <p.souz436@gmail.com>
2026-05-21 21:45:49 -0300

    Adicionando código da aula de hoje

--- aulas/aula04/src/pages/Perfil.jsx
+++ aulas/aula04/src/pages/Perfil.jsx
@@ -1,38 +1,92 @@
-import "./Menu.css";
-import { Link, NavLink } from "react-router";
-import { useAuthContext } from "../contexts/AuthContext";
+import { useParams } from "react-router";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
 
-function Menu() {
-  const usuarioId = 0; //pegou da API
-  const { logout , usuario} = useAuthContext();
-  const handleSair = () => {
-    logout();
+function Perfil() {
+  const { id } = useParams();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  // register() => {name="", onChange="", onBlur=""}
+  // handleSubit() => dispara a validação, informa ao React que mudou os campos
+
+  const salvar = (data) => console.log(data);
+
+  // buscar os dados do usuário
+
+  const regras = {
+    nome: {
+      required: "Nome é obrigatório",
+      minLength: { value: 3, message: "Nome tem que ter mais de 2 caracteres" },
+      maxLength: {
+        value: 100,
+        message: "Nome tem que ter no máximo 100 caracteres",
+      },
+    },
+    email: {
+      required: "E-mail é obrigatório",
+      pattern: {
+        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        message: "Email inválido",
+      },
+    },
+    nascimento: {
+      validate: {
+        dataMinima: (value) =>
+          Date.parse(`${value} 00:00:00 UTC`) >= new Date("01/01/1900 00:00:00 UTC").getTime() ||
+          "Data inferior",
+        dataMaxima: (value) =>
+          Date.parse(`${value} 00:00:00 UTC`) < new Date().getTime() || "Data superior",
+      },
+    },
+    telefone: {
+      min: { value: 0, message: "Telefone inválido" },
+      pattern: {
+        value: /^\+?\d{2,3} ?\(?\d{2}\)? ?\d{4,5}-\d{4}$/,
+        message: "Eehhhh cerular inválido",
+      },
+    },
   };
+
   return (
-    <nav>
-      <h4>{usuario.nome}</h4>
-      <h5>{usuario.email}</h5>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to={`/perfil/${usuarioId}`}>Perfil</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">Sobre</NavLink>
-        </li>
-        <li>
-          <NavLink to="/settings">Configurações</NavLink>
-        </li>
-        <li>
-          <Link to="/" onClick={handleSair}>
-            Sair
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <h1>Perfil do Usuário: {id}</h1>
+      <form onSubmit={handleSubmit(salvar)}>
+        <div>
+          <label htmlFor="nome">Nome</label>
+          <input type="text" id="nome" {...register("nome", regras.nome)} />
+          {errors?.nome && <p>{errors.nome.message}</p>}
+        </div>
+        <div>
+          <label htmlFor="email">E-mail</label>
+          <input type="email" id="email" {...register("email", regras.email)} />
+          {errors?.email && <p>{errors.email.message}</p>}
+        </div>
+        <div>
+          <label htmlFor="nascimento">Data de Nascimento</label>
+          <input
+            type="date"
+            id="nascimento"
+            {...register("nascimento", regras.nascimento)}
+          />
+          {errors?.nascimento && <p>{errors.nascimento.message}</p>}
+        </div>
+        <div>
+          <label htmlFor="telefone">Telefone</label>
+          <input
+            type="tel"
+            id="telefone"
+            {...register("telefone", regras.telefone)}
+          />
+          {errors?.telefone && <p>{errors.telefone.message}</p>}
+        </div>
+        <button>Salvar</button>
+      </form>
+    </>
   );
 }
 
-export default Menu;
+export default Perfil;
